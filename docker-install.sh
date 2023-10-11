e6ef35270ad96ea7c3cb33a00adcdf3ba2b19c0a
Niklas <niklaspein12@gmail.com>
2023-10-11 12:06:25 +0200

    docker-install use docker scripts

--- docker-install.sh
+++ docker-install.sh
@@ -1,28 +1,14 @@
 echo '###############################################'
-echo '#####    Add Docker''s official GPG key    #####'
-echo '###############################################'
-
-apt-get update
-apt-get install -y ca-certificates curl gnupg uidmap
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo '###############################################'
-echo '#####  Add the repository to Apt sources  #####'
-echo '###############################################'
-
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-
-echo '###############################################'
 echo '#####          Install Packages           #####'
 echo '###############################################'
 
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y curl uidmap
+
+echo '###############################################'
+echo '#####           Install Docker            #####'
+echo '###############################################'
+
+wget -qO- https://raw.githubusercontent.com/docker/docker-install/master/install.sh | sh
 
 if [[ ! -z "${http_proxy}" || ! -z "${https_proxy}" ]]; then
     echo '###############################################'
@@ -53,11 +39,4 @@ echo '###############################################'
 echo '#####      Configure non-root Access      #####'
 echo '###############################################'
 
-systemctl stop docker
-systemctl disable --now docker.service docker.socket
-runuser -u $SUDO_USER -- sh /usr/bin/dockerd-rootless-setuptool.sh install
-runuser -u $SUDO_USER -- systemctl --user start docker
-runuser -u $SUDO_USER -- systemctl --user enable docker
-loginctl enable-linger $(whoami)
-runuser -u $SUDO_USER -- echo "export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock" >> $HOME/.bashrc
-docker context use rootless
+wget -qO- https://raw.githubusercontent.com/docker/docker-install/master/rootless-install.sh | sh
